8cac796a92533b21dd707418baf42e761b239dbf
murugesh1994 <55495303+murugesh1994@users.noreply.github.com>
2021-09-04 13:00:40 +0530

    new commits

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// menu and menuoff function.
+function menu(){
+    document.getElementById("nav").style.left = "0";
+    alert("thanks for login");
+}
+function menuOff(){
+    document.getElementById("nav").style.left="-120%";
+}
+
+
+
